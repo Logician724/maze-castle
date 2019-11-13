4f52f6ff391145cacab068c8ac7bd8d4a16ffa0f
Logician724 <king2sairos2013@gmail.com>
2019-11-13 17:59:15 +0200

    fix: Change game state to static class

--- Assets/Scripts/gameState.cs
+++ Assets/Scripts/gameState.cs
@@ -2,57 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gameState : MonoBehaviour
+public static class GameState
 {
 
-    private bool hasTorch = false;
-    private bool leftRoomFirstTime = true;
-    private bool rightRoomFirstTime = true;
-
-    private void Awake()
-    {
-        DontDestroyOnLoad(this.gameObject);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public bool playerHasTorch()
-    {
-        return hasTorch;
-    }
-
-    public void setTorch(bool val)
-    {
-        this.hasTorch = val;
-    }
-
-    public bool getLeftRoomFirstTime()
-    {
-        return leftRoomFirstTime;
-    }
-
-    public void setLeftRoom(bool value)
-    {
-        this.leftRoomFirstTime = value;
-    }
-
-    public bool getReftRoomFirstTime()
-    {
-        return rightRoomFirstTime;
-    }
-
-    public void setRightRoom(bool val)
-    {
-        this.rightRoomFirstTime = val;
-    }
+    public bool hasTorch = false;
+    public bool leftRoomFirstTime = true;
+    public bool rightRoomFirstTime = true;
 }
