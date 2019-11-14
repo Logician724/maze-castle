e8129fbce43f0d37c87fc8f031b1fee2a95dab0f
Logician724 <king2sairos2013@gmail.com>
2019-11-14 23:41:52 +0200

    feat: Add pickup torch scene
    Note: The torch is picked up in the wrong hand LOL

---
+++ Assets/Scripts/PlayerWithTorch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWithTorch : MonoBehaviour
+{
+
+    public GameObject torch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        torch.SetActive(false);
+    }
+
+    // Update is called once per frames
+}
---
+++ Assets/Scripts/TorchSceneController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public class TorchSceneController : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject torch;
+    public GameObject door;
+    private Animator playerAnimator;
+    private PlayerWithTorch playerScript;
+
+    private string sceneState = "idle";
+    private bool isTorchPickedUp = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = player.GetComponent<Animator>();
+        playerScript = player.GetComponent<PlayerWithTorch>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (sceneState)
+        {
+            case "walkToTorch":
+                playerAnimator.SetTrigger("walkToTorch");
+                sceneState = "pickUpTorch";
+                break;
+            case "pickUpTorch":
+                if (Mathf.Abs(player.transform.position.z - torch.transform.position.z) <= 0.1)
+                {
+                    playerAnimator.SetTrigger("pickUpTorch");
+                    Invoke("AddTorchToPlayer", 1);
+                    sceneState = "temp";
+                }
+                break;
+            case "walkToDoor":
+                playerAnimator.SetTrigger("walkToDoor");
+                sceneState = "walkingToDoor";
+                break;
+            case "walkingToDoor":
+                if (Vector2.Distance(player.transform.position, door.transform.position) <= 6.5)
+                {
+                    door.GetComponent<Animator>().SetBool("isDoorOpen", true);
+                    sceneState = "final";
+                }
+                break;
+            default: break;
+        }
+    }
+
+    public void AddTorchToPlayer()
+    {
+        Destroy(torch);
+        playerScript.torch.SetActive(true);
+        sceneState = "walkToDoor";
+    }
+
+
+
+    public void PickUpTorch()
+    {
+        sceneState = "walkToTorch";
+    }
+
+    public void GoBack()
+    {
+        
+    }
+
+}
