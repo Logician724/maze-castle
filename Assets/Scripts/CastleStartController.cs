1e38fcf6de2c0025012e593f25e5d65802933176
Logician724 <king2sairos2013@gmail.com>
2019-11-15 14:40:21 +0200

    fix: Handle incorrect flow in torch scene

--- Assets/Scripts/CastleStartController.cs
+++ Assets/Scripts/CastleStartController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CastleStartController : MonoBehaviour
 {
@@ -80,6 +81,11 @@ public class CastleStartController : MonoBehaviour
     public void MoveLeft()
     {
         playerAnimator.SetTrigger("walk");
+    }
+
+    public void GoToTorchRoom()
+    {
+        
     }
 
 
--- Assets/Scripts/gameState.cs
+++ Assets/Scripts/gameState.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 public static class GameState
 {
 
+    public static string roomName = "castleStart";
     public static bool hasTorch = false;
     public static bool leftRoomFirstTime = true;
     public static bool rightRoomFirstTime = true;
